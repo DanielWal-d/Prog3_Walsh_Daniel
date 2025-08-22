c78ea6154caf849da58297fb8256bee2f0b8c133
DanielWal-d <2293545@cegepmontpetit.ca>
2025-08-22 13:35:12 -0400

    PremierProjet

---
+++ PremierProjet/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+package org.Walsh
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+println("Bonjour Walsh")
+
+
+}
